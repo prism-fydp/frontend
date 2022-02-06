e99f7e1330b4649dc778ff8dba940e89ada54e78
Connor Kingdon <connorkingdon1@gmail.com>
2022-02-05 20:29:12 -0500

    Add dropdown menu for IPFS
    Fix verification complaint about IPFS functionality not being used.
    Created simple menu items to perform all supported IPFS actions.

--- src/main/ipfs/ipfs.ts
+++ src/main/ipfs/ipfs.ts
@@ -4,117 +4,105 @@ import Request from './request_json';
 import Response from './response_json';
 import DaemonRequest from './classes';
 
-class IPFS {
-  HOST: string = 'localhost';
+const HOST = 'localhost';
+const PORT = 7777;
 
-  PORT: number = 7777;
+const createClient = (reject: (reason?: Error) => void): net.Socket => {
+  const client = new net.Socket();
 
-  client: net.Socket;
+  client.on('error', (err) => {
+    reject(err);
+  });
 
-  constructor() {
-    this.client = new net.Socket();
-    this.init();
-  }
+  client.connect(PORT, HOST, () => {
+    // console.log(`Connected to daemon (${this.HOST}:${this.PORT})`);
+  });
 
-  private init() {
-    this.client.connect(this.PORT, this.HOST, () => {
-      // console.log(`Connected to daemon (${this.HOST}:${this.PORT})`);
-    });
+  client.on('end', () => {
+    // console.log('Disconnected from daemon');
+  });
+  return client;
+};
 
-    this.client.on('end', () => {
-      // console.log('Disconnected from daemon');
-    });
-  }
+const parseResponse = (data: Buffer) => {
+  const dataStr: string = data.toString();
+  return new Response().deserialize(dataStr);
+};
 
-  /*
-   * Manually exit the connection with the daemon.
-   */
-  exit() {
-    this.client.end();
-  }
+/*
+ * ping the daemon. Mainly used for testing or verifying that the daemon is
+ * running. Returns whether the ping was correctly received.
+ */
+export function pingIPFS() {
+  return new Promise((resolve, reject) => {
+    const client = createClient(reject);
 
-  /*
-   * ping the daemon. Mainly used for testing or verifying that the daemon is
-   * running. Returns whether the ping was correctly received.
-   */
-  ping() {
-    return new Promise((resolve, reject) => {
-      const request = new Request();
-      request.Class = DaemonRequest.PING;
+    const request = new Request();
+    request.Class = DaemonRequest.PING;
 
-      if (!this.client.write(request.serialize())) {
-        reject(new Error('Failed to write request to TCP connection'));
+    if (!client.write(request.serialize())) {
+      reject(new Error('Failed to send request to daemon'));
+    }
+
+    client.on('data', (data) => {
+      const response = parseResponse(data);
+      if (response.Msg === 'Pong') {
+        resolve(true);
+      } else {
+        reject(
+          new Error(`Received response '${response.Msg}' and note 'Pong'`)
+        );
       }
-
-      this.client.on('data', (data) => {
-        const response = this.parseResponse(data);
-        if (response.Msg === 'Pong') {
-          resolve(true);
-        } else {
-          reject(
-            new Error(`Received response '${response.Msg}' and not 'Pong'`)
-          );
-        }
-      });
-
-      this.client.on('error', (err) => {
-        reject(err);
-      });
     });
-  }
 
-  /*
-   * add a file or directory to IPFS. The string returned on success is the
-   * CID of the added content.
-   */
-  add(path: string) {
-    return new Promise((resolve, reject) => {
-      const request = new Request();
-      request.Class = DaemonRequest.ADD;
-      request.Path = path;
-
-      if (!this.client.write(request.serialize())) {
-        reject(new Error('Failed to write request to TCP connection'));
-      }
-
-      this.client.on('data', (data) => {
-        resolve(this.parseResponse(data).Msg);
-      });
-
-      this.client.on('error', (err) => {
-        reject(err);
-      });
+    client.on('error', (err) => {
+      reject(err);
     });
-  }
+  });
+}
 
-  /*
-   * Get a piece of content from IPFS. Given the CID to fetch and the path to
-   * the directory to save the content into, the daemon will interact with
-   * IPFS and download as needed. Returned will be the path to the content
-   */
-  get(cid: string, path: string) {
-    return new Promise((resolve, reject) => {
-      const request = new Request();
-      request.Class = DaemonRequest.GET;
-      request.Cid = cid;
-      request.Path = path;
+/*
+ * add a file or directory to IPFS. The string returned on success is the CID
+ * of the added content.
+ */
+export function addIPFS(path: string) {
+  return new Promise((resolve, reject) => {
+    const client = createClient(reject);
 
-      if (!this.client.write(request.serialize())) {
-        reject(new Error('Failed to write request to TCP connection'));
-      }
+    const request = new Request();
+    request.Class = DaemonRequest.ADD;
+    request.Path = path;
 
-      this.client.on('data', (data) => {
-        resolve(this.parseResponse(data).Msg);
-      });
+    if (!client.write(request.serialize())) {
+      reject(new Error('Failed to send request to daemon'));
+    }
 
-      this.client.on('error', (err) => {
-        reject(err);
-      });
+    client.on('data', (data) => {
+      resolve(parseResponse(data).Msg);
     });
-  }
+  });
+}
 
-  parseResponse = (data: Buffer) => {
-    const dataStr: string = data.toString();
-    return new Response().deserialize(dataStr);
-  };
+/*
+ * Get a piece of content from IPFS. Given the CID to fetch and the path to
+ * the directory to save the content into, the daemon will interact with
+ * IPFS and download as needed. Returned will be the path to the content
+ */
+export function getIPFS(cid: string, path: string) {
+  return new Promise((resolve, reject) => {
+    const client = createClient(reject);
+
+    const request = new Request();
+    request.Class = DaemonRequest.GET;
+    request.Cid = cid;
+    request.Path = path;
+
+    if (!client.write(request.serialize())) {
+      reject(new Error('Failed to send request to daemon'));
+    }
+
+    client.on('data', (data) => {
+      resolve(parseResponse(data).Msg);
+    });
+  });
 }
