9e70409cff836ea9b7fa10cc98a7e36d9b0df0fe
Connor Kingdon <connorkingdon1@gmail.com>
2022-02-28 13:58:45 -0500

    Address PR comments

--- src/main/ipfs/ipfs.ts
+++ src/main/ipfs/ipfs.ts
@@ -4,17 +4,25 @@ import Request from './request_json';
 import Response from './response_json';
 import DaemonRequest from './classes';
 
-const HOST = 'localhost';
-const PORT = 7777;
+const DEFAULT_HOST = 'localhost';
+const DEFAULT_PORT = 7777;
 
-const createClient = (reject: (reason?: Error) => void): net.Socket => {
+/*
+ * Creates a client connection to the server and defines how to respond when
+ * this connection exits. Connected socket is returned
+ */
+const createClient = (
+  port: number,
+  host: string,
+  reject: (reason?: Error) => void
+): net.Socket => {
   const client = new net.Socket();
 
   client.on('error', (err) => {
     reject(err);
   });
 
-  client.connect(PORT, HOST, () => {
+  client.connect(port, host, () => {
     // console.log(`Connected to daemon (${this.HOST}:${this.PORT})`);
   });
 
@@ -24,6 +32,22 @@ const createClient = (reject: (reason?: Error) => void): net.Socket => {
   return client;
 };
 
+/*
+ *
+ */
+const issueRequest = (
+  client: net.Socket,
+  request: Request,
+  reject: (reason?: Error) => void
+) => {
+  if (!client.write(request.serialize())) {
+    reject(new Error('Failed to send request to daemon'));
+  }
+};
+
+/*
+ * Parses a response from the server into a typescript response structure
+ */
 const parseResponse = (data: Buffer) => {
   const dataStr: string = data.toString();
   return new Response().deserialize(dataStr);
@@ -33,16 +57,13 @@ const parseResponse = (data: Buffer) => {
  * ping the daemon. Mainly used for testing or verifying that the daemon is
  * running. Returns whether the ping was correctly received.
  */
-export function pingIPFS() {
+export function pingIPFS(port = DEFAULT_PORT, host = DEFAULT_HOST) {
   return new Promise((resolve, reject) => {
-    const client = createClient(reject);
+    const client = createClient(port, host, reject);
 
     const request = new Request();
     request.Class = DaemonRequest.PING;
-
-    if (!client.write(request.serialize())) {
-      reject(new Error('Failed to send request to daemon'));
-    }
+    issueRequest(client, request, reject);
 
     client.on('data', (data) => {
       const response = parseResponse(data);
@@ -54,10 +75,6 @@ export function pingIPFS() {
         );
       }
     });
-
-    client.on('error', (err) => {
-      reject(err);
-    });
   });
 }
 
@@ -65,17 +82,18 @@ export function pingIPFS() {
  * add a file or directory to IPFS. The string returned on success is the CID
  * of the added content.
  */
-export function addIPFS(path: string) {
+export function addIPFS(
+  path: string,
+  port = DEFAULT_PORT,
+  host = DEFAULT_HOST
+) {
   return new Promise((resolve, reject) => {
-    const client = createClient(reject);
+    const client = createClient(port, host, reject);
 
     const request = new Request();
     request.Class = DaemonRequest.ADD;
     request.Path = path;
-
-    if (!client.write(request.serialize())) {
-      reject(new Error('Failed to send request to daemon'));
-    }
+    issueRequest(client, request, reject);
 
     client.on('data', (data) => {
       resolve(parseResponse(data).Msg);
@@ -88,18 +106,20 @@ export function addIPFS(path: string) {
  * the directory to save the content into, the daemon will interact with
  * IPFS and download as needed. Returned will be the path to the content
  */
-export function getIPFS(cid: string, path: string) {
+export function getIPFS(
+  cid: string,
+  path: string,
+  port = DEFAULT_PORT,
+  host = DEFAULT_HOST
+) {
   return new Promise((resolve, reject) => {
-    const client = createClient(reject);
+    const client = createClient(port, host, reject);
 
     const request = new Request();
     request.Class = DaemonRequest.GET;
     request.Cid = cid;
     request.Path = path;
-
-    if (!client.write(request.serialize())) {
-      reject(new Error('Failed to send request to daemon'));
-    }
+    issueRequest(client, request, reject);
 
     client.on('data', (data) => {
       resolve(parseResponse(data).Msg);
