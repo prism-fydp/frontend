2c7c3537fb289f5c2862c37598d7c44bf4c30f2c
Connor Kingdon <connorkingdon1@gmail.com>
2022-03-01 12:26:49 -0500

    Restructure navigation
    Move NavButtons to be a separate component and make App responsible for
    all navigation. Required to fix testcase as it expects App to be fully
    renderable. Prior commit moved HashRouter to be a wrapper of App.
    NavButtons must be created in a file separate to the Router instance.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,37 +1,25 @@
 import './App.css';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+import NavButton from './components/nav_button';
 import MarkdownEditor from './pages/editor';
 import Home from './pages/home';
 import MarkdownReader from './pages/reader';
 
-// window.electron.ipcRenderer.once('ipc-example', (arg) => {
-//   // console.log(arg);
-// });
-
-// window.electron.ipcRenderer.myPing();
-
-const NavButton = (path: string, name: string) => {
-  const navigate = useNavigate();
-  return (
-    <button type="button" onClick={() => navigate(path)}>
-      {name}
-    </button>
-  );
-};
-
 export default function App() {
   return (
-    <div className="app">
+    <HashRouter>
       <div className="nav-buttons">
-        {NavButton('/md-reader', 'Read')}
-        {NavButton('/md-editor', 'Write')}
-        {NavButton('/', 'Home')}
+        <NavButton path="/md-reader" name="Read" />
+        <NavButton path="/md-editor" name="Write" />
+        <NavButton path="/" name="Home" />
       </div>
       <Routes>
         <Route path="/md-reader" element={MarkdownReader()} />
         <Route path="/md-editor" element={MarkdownEditor()} />
         <Route path="/" element={Home()} />
       </Routes>
-    </div>
+    </HashRouter>
   );
 }
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,10 +1,4 @@
 import { render } from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 
-render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root')
-);
+render(<App />, document.getElementById('root'));
